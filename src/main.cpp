e99f5a8510411d45b52eac516b8bd29ab6290d90
mjt <you@example.com>
2025-04-08 15:06:32 -0400

    all working

--- src/main.cpp
+++ src/main.cpp
@@ -32,7 +32,7 @@ Figures figArray[] = {
     Figures(3, 135, 122, 0, 0, 122, 1, 1, 0),
     Figures(4, 110, 122, 0, 0, 122, 0, 0, 1),
     Figures(5, 110, 0, 0, 122, 122, 1, 0, 1),
-    Figures(6, 175, 76, 46, 0, 122, 1, 1, 1)};
+    Figures(6, 175, 76, 46, 0, 122, 0, 1, 1)};
 
 void setup()
 {
@@ -49,8 +49,6 @@ void setup()
   delay(2000); // todo modify
   LEDobj.LEDsAllOff();
 
-  Serial.println("after startup flash...");
-
   PhotoResObj.setPinMode();
 }
 
@@ -58,25 +56,15 @@ void setup()
 
 void loop()
 {
-  Serial.println(" START LOOP ");
-
-  for (int i=0; i < NUMBER_OF_FIGS; i++)
-
+  for (int i = 0; i < NUMBER_OF_FIGS; i++)
   {
 
- int   photoVal = PhotoResObj.readLightLevel(figArray[i]);
-
-    Serial.print(" Figure: ");
-    Serial.print(figArray[i].getFigNum() );
-    Serial.print(" -photo value: ");
-    Serial.print(photoVal);
-
+    int photoVal = PhotoResObj.readLightLevel(figArray[i]);
     unsigned long timeRead = millis();
-
-        switch (figArray[i].getFigState())
+    
+    switch (figArray[i].getFigState())
     {
     case LED_OFF_NOT_TALKING: //  do nothing
-      Serial.print(" -start case 0 ");
 
       // did figure start talking
       if (photoVal > figArray[i].getTALKING_MIN())
@@ -90,14 +78,9 @@ void loop()
         figArray[i].setLastKnowntalkTime(timeRead);
         figArray[i].setFigState(LED_ON_TALKING);
       }
-
-      Serial.print("-exit case 0 state:");
-      Serial.println(figArray[i].getFigState());
-
       break;
 
     case LED_ON_TALKING: //  Figure talking
-      Serial.print("in case 1...");
 
       if (photoVal > figArray[i].getTALKING_MIN())
       {
@@ -107,14 +90,12 @@ void loop()
       {
         figArray[i].setFigState(LED_KEEP_ON);
       }
-      Serial.print("-exit case 1 state:");
-      Serial.println(figArray[i].getFigState());
+
       break;
 
     case LED_KEEP_ON: //  Figure stay lit for a bit after stop talking
-    
-    Serial.print(" -start case 2 "); 
-    if (photoVal > figArray[i].getTALKING_MIN())
+
+      if (photoVal > figArray[i].getTALKING_MIN())
       {
         figArray[i].setLastKnowntalkTime(timeRead);
         figArray[i].setFigState(LED_ON_TALKING);
@@ -126,29 +107,25 @@ void loop()
         figArray[i].setDimmingStep(0);
         figArray[i].setDimStartTime(timeRead);
       }
-      Serial.print("-exit case 2 state:");
-      Serial.println(figArray[i].getFigState());
-
       break;
 
-    case LED_DIMMING:  
-    Serial.print(" -start case 3 "); 
+    case LED_DIMMING:
 
-    //  Figure stopped talking, led dimming
+      //  Figure stopped talking, led dimming
       if (photoVal > figArray[i].getTALKING_MIN()) // check to if fig started talking
       {
         figArray[i].setLastKnowntalkTime(timeRead);
         figArray[i].setFigState(LED_ON_TALKING);
       }
-     
-     if (timeRead - figArray[i].getLastKnowntalkTime() < delayLEDon)
-     {
-      /* code */  // still in delay period , do nothing  // todo clean up logic to remove this dead zone
-     }
-          
+
+      if (timeRead - figArray[i].getLastKnowntalkTime() < delayLEDon)
+      {
+        /* code */ // still in delay period , do nothing  // todo clean up logic to remove this dead zone
+      }
+
       else
-      
-      if (figArray[i].getDimmingStep() < dimmingSteps)
+
+          if (figArray[i].getDimmingStep() < dimmingSteps)
       {
         int brightness = 255 * (dimmingSteps - figArray[i].getDimmingStep()) / dimmingSteps;
         LEDobj.setFigIllum(figArray[i].getFigNum(),
@@ -156,27 +133,20 @@ void loop()
                            figArray[i].getFigColorGreen() * brightness / 255,
                            figArray[i].getFigColorBlue() * brightness / 255,
                            figArray[i].getFigColorWhite() * brightness / 255);
-      
 
-      if (timeRead - figArray[i].getDimStartTime() > (dimmingDuration / dimmingSteps))
-      {
-        figArray[i].incDimmingStep();
-        figArray[i].setDimStartTime(timeRead);
+        if (timeRead - figArray[i].getDimStartTime() > (dimmingDuration / dimmingSteps))
+        {
+          figArray[i].incDimmingStep();
+          figArray[i].setDimStartTime(timeRead);
+        }
       }
-    }
       else
 
       {
         LEDobj.setFigIllum(figArray[i].getFigNum(), 0, 0, 0, 0);
         figArray[i].setFigState(LED_OFF_NOT_TALKING);
       }
-
-      Serial.print("-exit case 3 state:");
-      Serial.println(figArray[i].getFigState());
       break;
     }
   }
-
-  Serial.println("end of loop...");
 }
-
